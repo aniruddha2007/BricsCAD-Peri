27aec69b76449b7bc94b3207fff73dacfb2dc9d4
daniel gallardo <daniel-gallardo@live.com>
2024-08-10 23:57:21 +0800

    - Changed position of bracket so it doesn't colide with wingnut - Fixing clockwise/counterclockwise (in progress)

--- Scafold/PlaceBracket-PP.h
+++ Scafold/PlaceBracket-PP.h
@@ -10,6 +10,8 @@
 #include "rxregsvc.h"
 #include "geassign.h"
 #include <string>
+#include <vector>
+#include "gepnt3d.h"
 
 struct BlockInfo {
     std::wstring blockName;
@@ -24,4 +26,21 @@ public:
     static void addTextAnnotation(const AcGePoint3d& position, const wchar_t* text);
     static void placeAsset(const AcGePoint3d& position, const wchar_t* blockName, double rotation = 0.0, double scale = 1.0);
     static void placeBrackets();
+
+private:
+    static std::vector<AcGePoint3d> detectPolylines();
+
+    // Comparator for AcGePoint3d to be used in the map
+    struct Point3dComparator {
+        bool operator()(const AcGePoint3d& lhs, const AcGePoint3d& rhs) const {
+            if (lhs.x != rhs.x)
+                return lhs.x < rhs.x;
+            if (lhs.y != rhs.y)
+                return lhs.y < rhs.y;
+            return lhs.z < rhs.z;
+        }
+    };
+
+    // Static member to hold the wall mapping
+    static std::map<AcGePoint3d, std::vector<AcGePoint3d>, Point3dComparator> wallMap;
 };
